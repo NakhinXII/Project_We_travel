bfd1a2e2276b797b0d891323897468a7f6454883
NakhinXII <110448735+NakhinXII@users.noreply.github.com>
2023-10-31 22:19:40 +0700

    Initial commit

---
+++ we_travel/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAxxPID2E8rpT3RCkvg6zPPICjnm7pd_X8",
+  authDomain: "we-travel-49a89.firebaseapp.com",
+  projectId: "we-travel-49a89",
+  storageBucket: "we-travel-49a89.appspot.com",
+  messagingSenderId: "1085172250464",
+  appId: "1:1085172250464:web:f65f02b066a4a158499005"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
